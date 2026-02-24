e317603e8cc43b33f199471758c273b63ee5ef5c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-02-24 14:44:19 -0400

    flutter

--- 005. Sección 5: Control de flujo/012. Tarea 6: Menu de opciones iterativo/LeetCode/src/main/java/Exercise.java
+++ 005. Sección 5: Control de flujo/012. Tarea 6: Menu de opciones iterativo/LeetCode/src/main/java/Exercise.java
@@ -52,18 +52,21 @@ import java.util.Map;
 
 public class MantenedorProductos {
     public static void main(String[] args) {
+
         var opciones = Map.of("Actualizar", 1, "Eliminar", 2, "Agregar", 3, "Listar", 4, "Salir", 5);
+        
         Object[] opArreglo = opciones.keySet().toArray();
+        
         Integer sel = 0;
 
         do {
+        
             Object opcion = JOptionPane.showInputDialog(null, "Seleccione una Opción", "Mantenedor", 1, null, opArreglo, opArreglo[0]);
             
             if (opcion == null) break; // Salida rápida si cancela
 
             sel = opciones.get(opcion.toString());
             
-            // Usamos Switch Expression para asignar el mensaje en una sola línea
             String msj = switch (sel) {
                 case 1 -> "Usuario actualizado";
                 case 2 -> "Usuario eliminado";
